26922da204a430bb995c7fed36c6ab953242aee9
dorian.pinaud <dorian.pinaud@gmail.com>
2024-09-16 18:39:53 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,7 +8,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
     connection.on("data", () => {
         connection.write("+PONG\r\n");
-    })
+    });
 });
 
 server.listen(6379, "127.0.0.1");
