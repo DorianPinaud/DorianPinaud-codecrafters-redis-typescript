a1d5a11c80d969f455fb39152e9e74af4027dfa6
dorian.pinaud <dorian.pinaud@gmail.com>
2024-09-16 23:33:25 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,35 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+function encodesBulkString(str: string): string {
+    return `$${str.length}\r\n${str}\r\n`
+}
+
+function splitBulkString(str: string): string[] {
+    const regex: RegExp = /^\*\d+|[a-zA-Z]+$/gm;
+    const search = str.match(regex)
+    if (search === null) {
+        return [str];
+    }
+    else {
+        return search.slice(1);
+    }
+}
+
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
-    connection.on("data", () => {
-        connection.write("+PONG\r\n");
+    connection.on("data", (data: ArrayBuffer) => {
+        const decoder = new TextDecoder()
+        const string_data = decoder.decode(data)
+        const array_data = splitBulkString(string_data)
+        console.log(array_data)
+        if (array_data[0].toUpperCase() === "ECHO" && array_data.length === 2) {
+            connection.write(encodesBulkString(array_data[1]));
+        }
+        if (array_data[0].toUpperCase() == "PING") {
+            connection.write(encodesBulkString("PONG"))
+        }
     });
 });
 
